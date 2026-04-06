57bde0c2b25304c5a030f7c5c2d448f15390406a
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-04-07 00:34:47 +0800

    Теперь объект двигается с помощью матрицы трансформации. Добавили игрока. Выводим несколько объектов)

--- WinApp/WinFormsApp1/Form1.cs
+++ WinApp/WinFormsApp1/Form1.cs
@@ -5,17 +5,31 @@ namespace WinFormsApp1
     public partial class Form1 : Form
     {
         MyRectangle myRect;
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
         public Form1()
         {
             InitializeComponent();
-            myRect = new MyRectangle(0, 0, 0);
+            player = new Player(pbMain.Width/2, pbMain.Height/2,0);
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+
+            objects.Add(marker);
+            objects.Add(player);
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
             g.Clear(Color.White);
-            myRect.Render(g);
+
+            foreach(var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
         }
     }
 }
--- WinApp/WinFormsApp1/Objects/BaseObject.cs
+++ WinApp/WinFormsApp1/Objects/BaseObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing.Drawing2D;
 namespace WinFormsApp1.Objects
 {
     class BaseObject
@@ -17,9 +17,18 @@ namespace WinFormsApp1.Objects
             Y = y;
             Angle = angle;
         }
+        public Matrix GetTransform()
+        {
+            var matrix = new Matrix();
+            matrix.Translate(X, Y);
+            matrix.Rotate(Angle);
+
+            return matrix;
+        }
         public virtual void Render(Graphics g)
         {
-            // тут пусто
+            g.FillRectangle(new SolidBrush(Color.Yellow), -25, -15, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red, 2), -25, -15, 50, 30);
         }
     }
 }
